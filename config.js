c260324d1afbfc165c5cad0a2764181a7a50066d
enderson <endergrimaldos26@gmail.com>
2023-10-17 08:08:10 -0400

    cambios nuevos realizados

--- config.js
+++ config.js
@@ -2,8 +2,5 @@ console.log("primer paso")
 console.log("primer paso")
 console.log("primer paso")
 console.log("primer paso")
-    <<<<<<<< < Temporary merge branch 1
 console.log("primer paso")
-=========
->>>>>>>>> Temporary merge branch 2
 console.log("primer paso")
